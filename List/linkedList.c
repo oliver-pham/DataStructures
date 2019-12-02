5be7a9853069dae6369a68d0ed76e9f262b97d00
ph-thng <phuthong01@gmail.com>
2019-12-01 23:44:10 -0500

    Implemented Queue using Linked List

--- List/linkedList.c
+++ List/linkedList.c
@@ -13,6 +13,22 @@ List *initializeList(int item)
     return l;
 }
 
+Queue *initializeQueue(List **head)
+{
+    Queue *q;
+    List *p = &head;
+
+    if (p = NULL) return p;
+    while (p->next != NULL)
+    {
+        p = p->next;
+    }
+
+    q->head = &head;
+    q->tail = p;
+    return q;
+}
+
 List *searchList(List *l, int item)
 {
     if (l == NULL) return NULL;
@@ -28,7 +44,7 @@ List *searchList(List *l, int item)
     }
 }
 
-void insertList(List **head, int item)
+void push(List **head, int item)
 {
     List *l;
 
@@ -37,6 +53,72 @@ void insertList(List **head, int item)
     l->next = *head;
     *head = l;
     printf("Inserted a list successfully.\n");
+}
+
+List *pop(List **head)
+{
+    List *p;
+
+    p = &head;
+    if (p != NULL)
+    {
+        *head = p->next;
+        printf("Popped a list successfully.\n");
+        return p;
+    }
+    else
+    {
+        printf("Empty List!\n");
+        return p;
+    }
+}
+
+void enqueue(Queue *q, int item)
+{
+    List *l;
+
+    l = malloc(sizeof(List));
+    l->item = item;
+    l->next = NULL;
+    q->tail->next = l;
+    printf("Enqueued a list successfully.\n");
+}
+
+List *dequeue(Queue *q)
+{
+    List *p;
+
+    p = q->head;
+    q->head = q->head->next;
+    printf("Dequeued a list successfully.\n");
+    return p;
+}
+
+void deleteList(List **head, int item)
+{
+    List *p;        // item pointer
+    List *pred;     // predecessor pointer
+    List *searchList(), *predecessorList();
+
+    p = searchList(*head, item);
+    if (p != NULL)
+    {
+        pred = predecessorList(*head, item);
+        if (pred == NULL)
+        {
+            *head = p->next;
+        }
+        else
+        {
+            pred->next = p->next;
+        }
+        free(p);
+        printf("Item deleted successfully.\n");
+    }
+    else
+    {
+        printf("Error: item not found.\n");
+    }
 }
 
 void printList(List *l)
@@ -67,32 +149,5 @@ List *predecessorList(List *l, int item)
     else
     {
         return(predecessorList(l->next, item));
-    }
-}
-
-void deleteList(List **head, int item)
-{
-    List *p;        // item pointer
-    List *pred;     // predecessor pointer
-    List *searchList(), *predecessorList();
-
-    p = searchList(*head, item);
-    if (p != NULL)
-    {
-        pred = predecessorList(*head, item);
-        if (pred == NULL)
-        {
-            *head = p->next;
-        }
-        else
-        {
-            pred->next = p->next;
-        }
-        free(p);
-        printf("Item deleted successfully.\n");
-    }
-    else
-    {
-        printf("Error: item not found.\n");
     }
 }
--- List/linkedList.h
+++ List/linkedList.h
@@ -3,3 +3,9 @@ typedef struct List
     int item;
     struct List *next;
 } List;
+
+typedef struct Queue
+{
+    List *head;
+    List *tail;
+} Queue;
